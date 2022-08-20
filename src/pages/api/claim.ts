fbfe5ff87c9eab0c1c5fa106bba3317027f9afff
flywill <111021470+0xflywill@users.noreply.github.com>
2022-08-19 19:49:22 -0500

    working 100%

--- src/pages/api/claim.ts
+++ src/pages/api/claim.ts
@@ -5,7 +5,21 @@ const POAP_URL = 'https://welook.io/mint/cwb8p5'
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	const { signal, action_id, merkle_root, nullifier_hash, proof } = req.body
 
-	if (true) {
+	const response = await fetch('https://developer.worldcoin.org/api/v1/verify', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			signal,
+			action_id,
+			merkle_root,
+			nullifier_hash,
+			proof,
+		}),
+	})
+
+	if (response.ok) {
 		return res.status(200).json({ url: POAP_URL })
 	}
 	return res.status(400).json(await response.json())
